3bd815c7ce1bd7c15ea6947941a843bdccd24eee
AlpiGun5 <alperen.tf2@gmail.com>
2024-09-24 17:21:51 +0200

    Modify G interactive script

--- src/solvers/RunnerG2022.bash
+++ src/solvers/RunnerG2022.bash
@@ -1,3 +1,4 @@
 javac ProblemG2022Solver.java
 py ../../data2022/guessingprimes/attachments/testing_tool.py \
     -f ../../data2022/guessingprimes/data/secret/"$1".in java ProblemG2022Solver
+rm ProblemG2022Solver.class
