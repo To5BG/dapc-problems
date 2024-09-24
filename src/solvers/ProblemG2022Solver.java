cedb028d34eebc581b6f9e9ed170a3500484a4d4
AlpiGun5 <alperen.tf2@gmail.com>
2024-09-24 17:14:39 +0200

    Optimize solution to G, 2022

--- src/solvers/ProblemG2022Solver.java
+++ src/solvers/ProblemG2022Solver.java
@@ -1,6 +1,4 @@
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @SuppressWarnings("all")
 public class ProblemG2022Solver {
@@ -9,27 +7,51 @@ public class ProblemG2022Solver {
     // Generate all primes in a hashset, and reduce the space based on the white/yellow/green indicators
     // One option is to store the guesses as strings, and use regex to reduce the space
     // We have both 'necessary' and 'forbidden' patterns, and check each number on all of these
+    // More performant -> Store primes in a map of contained digits to set of numbers
+    // Update candidate digits at each position separately after each guess
     static String[] firstGuesses = new String[]{"10597", "24683"};
-    static int fi = 0;
+    static final int DIGITS = 5;
+    static final int TOTAL_ROUNDS = 6;
 
     public static void main(String[] args) {
-        Set<String> primes = new HashSet<>();
-        for (int i = 10001; i < 99999; i++) if (isPrime(i)) primes.add(String.valueOf(i));
-
+        Map<Set<Character>, Set<String>> primes = new HashMap<>();
+        for (int i = (int) Math.pow(10, DIGITS - 1); i <= (int) Math.pow(10, DIGITS); i++)
+            if (isPrime(i)) {
+                Set<Character> chars = new HashSet<>();
+                String number = String.valueOf(i);
+                for (int d = 0; d < DIGITS; d++) chars.add(number.charAt(d));
+                if (!primes.containsKey(chars)) primes.put(chars, new HashSet<>());
+                primes.get(chars).add(number);
+            }
         Scanner sc = new Scanner(System.in);
         int rounds = sc.nextInt();
+        round:
         for (int r = 0; r < rounds; r++) {
-            Set<String> currPrimes = new HashSet<>(primes);
-            for (int j = 0; j < 6; j++) {
-                String nextGuess = nextGuess(currPrimes);
+            List<Set<Character>> charSet = new ArrayList<>();
+            charSet.add(new HashSet<>(Set.of('1', '2', '3', '4', '5', '6', '7', '8', '9')));
+            for (int i = 0; i < DIGITS - 2; i++)
+                charSet.add(new HashSet<>(Set.of('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')));
+            charSet.add(new HashSet<>(Set.of('1', '3', '5', '7', '9')));
+            Set<Character> possibleChars = new HashSet<>();
+            for (int i = 0; i < firstGuesses.length; i++) {
+                String guess = firstGuesses[i];
+                System.out.println(guess);
+                System.out.flush();
+                String res = sc.next();
+                if (res.equals("ggggg")) continue round;
+                for (int j = 0; j < DIGITS; j++) if (res.charAt(j) != 'w') possibleChars.add(guess.charAt(j));
+                updateSet(charSet, guess, res);
+            }
+            Set<String> candidates = new HashSet<>(primes.get(possibleChars));
+            updateCandidates(candidates, charSet);
+            for (int j = 0; j < TOTAL_ROUNDS - firstGuesses.length; j++) {
+                String nextGuess = candidates.iterator().next();
                 System.out.println(nextGuess);
                 System.out.flush();
                 String res = sc.next();
-                if (res.equals("ggggg")) {
-                    fi = 0;
-                    break;
-                }
-                updatePrimes(primes, currPrimes, nextGuess, res);
+                if (res.equals("ggggg")) break;
+                updateSet(charSet, nextGuess, res);
+                updateCandidates(candidates, charSet);
             }
         }
     }
@@ -39,70 +61,36 @@ public class ProblemG2022Solver {
         return true;
     }
 
-    public static void updatePrimes(Set<String> primes, Set<String> currPrimes, String guess, String res) {
-        currPrimes.remove(guess);
-        List<String> forbiddenStr = new ArrayList<>(), necessaryStr = new ArrayList<>();
-        Map<Integer, List<Integer>> green = new HashMap<>();
-        Map<Integer, Integer> greenC = new HashMap<>(), yellowC = new HashMap<>(), whiteC = new HashMap<>();
-        for (int i = 0; i < 5; i++) {
-            char resultChar = res.charAt(i);
-            int guessDigit = guess.charAt(i) - 48;
-            if (resultChar == 'g') {
-                if (!green.containsKey(guessDigit)) green.put(guessDigit, new ArrayList<>());
-                green.get(guessDigit).add(i);
-                greenC.merge(guessDigit, 1, Integer::sum);
-            } else if (resultChar == 'y') {
-                yellowC.merge(guessDigit, 1, Integer::sum);
-                // Cannot have the digit over a yellow mark
-                forbiddenStr.add(".".repeat(i) + guessDigit + ".".repeat(4 - i));
-            } else whiteC.merge(guessDigit, 1, Integer::sum);
-        }
-        for (Map.Entry<Integer, List<Integer>> e : green.entrySet()) {
-            StringBuilder sb = new StringBuilder(".".repeat(5));
-            for (int i : e.getValue()) sb.setCharAt(i, (char) (e.getKey() + 48));
-            // Must have the digit over green marks
-            necessaryStr.add(sb.toString());
-        }
-        for (Map.Entry<Integer, Integer> e : yellowC.entrySet()) {
-            int count = e.getValue() + greenC.getOrDefault(e.getKey(), 0);
-            // Must contain the digit as many times as (yellow + green) marks
-            necessaryStr.add((".*" + e.getKey()).repeat(count) + ".*");
-        }
-        for (Map.Entry<Integer, Integer> e : whiteC.entrySet()) {
-            int count = e.getValue() + greenC.getOrDefault(e.getKey(), 0) +
-                    yellowC.getOrDefault(e.getKey(), 0);
-            // Must not have the digit as many times as (white + yellow + green) marks
-            // Aka if no green or yellow, just one white, then it is not contained at all (<=> cannot have it once)
-            forbiddenStr.add((".*" + e.getKey()).repeat(count) + ".*");
-        }
-        List<Pattern> forbidden = forbiddenStr.stream().map(Pattern::compile).toList();
-        List<Pattern> necessary = necessaryStr.stream().map(Pattern::compile).toList();
-        outer:
-        for (String n : primes) {
-            if (!currPrimes.contains(n)) continue;
-            for (Pattern p : necessary) {
-                Matcher mat = p.matcher(n);
-                if (!mat.find()) {
-                    currPrimes.remove(n);
-                    continue outer;
-                }
+    public static void updateSet(List<Set<Character>> charSet, String guess, String res) {
+        int[] doesNotContainDigitFlag = new int[10];
+        for (int i = 0; i < DIGITS; i++) {
+            char c = res.charAt(i), d = guess.charAt(i);
+            if (c == 'w') {
+                if (doesNotContainDigitFlag[d - 48] == 0) doesNotContainDigitFlag[d - 48] = 1;
+                charSet.get(i).remove(d);
+            } else if (c == 'y') {
+                doesNotContainDigitFlag[d - 48] = -1;
+                charSet.get(i).remove(d);
+            } else {
+                doesNotContainDigitFlag[d - 48] = -1;
+                for (char j = 48; j < 58; j++) if (j != d) charSet.get(i).remove(j);
             }
-            for (Pattern p : forbidden) {
-                Matcher mat = p.matcher(n);
-                if (mat.find()) {
-                    currPrimes.remove(n);
+        }
+        for (int i = 0; i < 10; i++)
+            if (doesNotContainDigitFlag[i] == 1)
+                for (int j = 0; j < DIGITS; j++) charSet.get(j).remove(i);
+    }
+
+    public static void updateCandidates(Set<String> candidates, List<Set<Character>> charSet) {
+        List<String> remove = new ArrayList<>();
+        for (String c : candidates) {
+            for (int i = 0; i < DIGITS; i++) {
+                if (!charSet.get(i).contains(c.charAt(i))) {
+                    remove.add(c);
                     break;
                 }
             }
         }
-    }
-
-    public static String nextGuess(Set<String> currPrimes) {
-        if (fi < 2) return firstGuesses[fi++];
-        Random rand = new Random();
-        int randomIndex = rand.nextInt(currPrimes.size());
-        Iterator<String> iter = currPrimes.iterator();
-        for (int i = 0; i < randomIndex; i++) iter.next();
-        return iter.next();
+        for (String c : remove) candidates.remove(c);
     }
 }
